0806cd13c45080ee4466eb1c1a05c9b5379d5906
Pierre Axelsson <pierre.axelsson@outlook.com>
2023-05-29 15:26:55 +0200

    Added plugins and ViewModel

---
+++ DeluxeEdit.DefaultPlugins/ViewModel/MainEditViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeluxeEdit.DefaultPlugins.ViewModel
+{
+    public class MainEditViewModel: INotifyPropertyChanged
+    {
+        private MainEdit mainEdit;
+
+        // Declare the event
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MainEditViewModel()
+        {
+            mainEdit = new MainEdit();
+            
+        }
+
+
+  //      OnPropertyChange();
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    
+        public void Show()
+        { 
+            
+        }
+    }
+}
