7b0b448c78fac53199d06c49a8208fbe5fd90e94
Pierre Axelsson <pierre.axelsson@outlook.com>
2024-02-01 21:32:27 +0100

    Separated classes in files

---
+++ Model/CustomMenu.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class CustomMenu
+    {
+        public string Header { get; set; }
+        public List<CustomMenuItem> MenuItems { get; set; }
+
+
+    }
+}
--- Model/CustomMenuItem.cs
+++ Model/CustomMenuItem.cs
@@ -4,18 +4,11 @@ using System.Collections.Generic;
 
 namespace Model
 {
-    public class CustomMenu
-    {
-        public string Header { get; set; }
-        public List<CustomMenuItem> MenuItems { get; set; }
-
-
-    }
 
     public class CustomMenuItem
 
     {
         public string Title { get; set; }
-    public Action OnAction { get; set; }
-q    }
+        public Action OnAction { get; set; }
+    }
 }
