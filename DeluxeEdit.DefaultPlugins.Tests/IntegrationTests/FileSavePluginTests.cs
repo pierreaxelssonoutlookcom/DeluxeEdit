6c25d3d49e3ea48c087678eae2b20317706348c4
Pierre Axelsson <pierre.axelsson@outlook.com>
2024-08-06 04:11:03 +0200

    Tests now work

--- DeluxeEdit.DefaultPlugins.Tests/IntegrationTests/FileSavePluginTests.cs
+++ DeluxeEdit.DefaultPlugins.Tests/IntegrationTests/FileSavePluginTests.cs
@@ -2,48 +2,65 @@
 using DefaultPlugins;
 using Model;
 using Xunit;
+using System.IO;
 
 namespace DeluxeEdit.DefaultPlugins.Tests.IntegrationTests
 {
     public class FileSavePluginTests
     {
         public static string TempDir = "C:/temp";
-        public static string TestFile = TempDir + "/testsave.txt";
-        public static string TestFile2 = TempDir + "/testsave.txt";
+        public static string TestFile = $"{TempDir}/testfile__{Guid.NewGuid()}.txt";
+        public static string TestFile2 = $"{TempDir}/testfile2__{Guid.NewGuid()}.txt";
 
         [Fact]
         public async void FileSavePluginTest()
         {
             var plugin = AllPlugins.InvokePlugin(PluginType.FileSaveAs) as FileSavePlugin;
 
-            var expected = "ninjaåäÖ";
-            if (File.Exists(TestFile)) File.Delete(TestFile);
-            File.WriteAllText(TestFile, "ninjaåäÖ", Encoding.UTF8);
+            var expected = "ninjaåäÖ\r\n";
+            File.AppendAllLines(TestFile,new List<string> { "ninjaåäÖ" },  Encoding.UTF8);
 
             plugin.OpenEncoding=Encoding.UTF8;
             await plugin.Perform(
 
-               new ActionParameter(TestFile, "ninjaåäÖ" )
+               new ActionParameter(TestFile, "ninjaåäÖ")
               , null );
-            
-            
-            var actual=File.ReadAllText(TestFile, Encoding.UTF8);
-            Assert.Equal(expected, actual);
+
+            File.Copy(TestFile, TestFile2, true);
+
+
+
+            var actual =File.ReadAllText(TestFile2, Encoding.UTF8);
+
+            File.Delete(TestFile);
+            File.Delete(TestFile2);
+            Assert.Equal(expected , actual);
+
+  
         }
         [Fact]
          public async void FileSavePluginTestSimple()
         {
             var plugin = AllPlugins.InvokePlugin(PluginType.FileSaveAs) as FileSavePlugin;
 
-            var expected = "ninja";
-            if (File.Exists(TestFile2)) File.Delete(TestFile2);
-            File.WriteAllText(TestFile2, "ninja");
+            var expected = "ninja" + Environment.NewLine;
+            File.AppendAllLines(TestFile, new List<string> { "ninja" }, Encoding.UTF8);
+
+            // if (File.Exists(TestFile2)) File.Delete(TestFile2);
+
             plugin.OpenEncoding = null;
             await  plugin.Perform(
-                new ActionParameter(TestFile2, "ninja"),null);
-            var actual = File.ReadAllText(TestFile);
+               new ActionParameter(TestFile, "ninja"),null);
+            File.Copy(TestFile, TestFile2, true);
+
+
+
+            var actual = File.ReadAllText(TestFile2);
+            File.Delete(TestFile);
+            File.Delete(TestFile2);
 
             Assert.Equal(expected, actual);
+
         }
 
 
