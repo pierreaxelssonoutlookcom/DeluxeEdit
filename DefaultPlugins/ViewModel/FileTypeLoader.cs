69108ba9fd06ff99cc7a7013bdc7d36a7c3f57bc
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-01-14 00:50:16 +0100

    Systax hiligting work by using HighlightingManager.Instance

--- DefaultPlugins/ViewModel/FileTypeLoader.cs
+++ DefaultPlugins/ViewModel/FileTypeLoader.cs
@@ -7,21 +7,23 @@ using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.Editing;
 using ICSharpCode.AvalonEdit.Document;
+using System.IO;
+using System.Collections.ObjectModel;
+using Extensions;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 
 namespace ViewModel
 {
     public class FileTypeLoader
     {
-        private HighlightingManager manager;
+
         public static List<FileTypeItem> AllFileTypes { get; set; }= LoadFileTypes();
         public static string CurrentPath { get; set; }=String.Empty;
         public TextEditor CurrentText { get; set; } = new TextEditor();
-        public TextArea CurrentArea { get; set; } = new TextEditor().TextArea;  
+        public TextArea CurrentArea { get; set; } = new TextEditor().TextArea;
+    //    public FileTypeItem? CurrentFileItem { get; set; } = new FileTypeItem();
+        public IHighlightingDefinition? CurrentDefinition { get; private set; }
 
-        public FileTypeLoader()
-        {
-            manager = new HighlightingManager();
-        }
         public static FileTypeItem? GetFileTypeItemByMenu(string menuTitle)
         {
             var result = AllFileTypes.FirstOrDefault(p => p.ToString() == menuTitle && menuTitle.StartsWith("As "));
@@ -35,35 +37,32 @@ namespace ViewModel
 
 
 
-
-
-
-        public void LoadCurrent(string path)
-        {
-
-
-            CurrentText = new TextEditor();
-            CurrentArea = CurrentText.TextArea;
-            CurrentArea.MinHeight = 500;
-            CurrentArea.MinWidth = 1000;
-
-            //CurrentDocument= CurrentText.Document;
-            CurrentText.IsReadOnly = false;
-           
  
-            var suggestededDefinition = manager.GetDefinitionByExtension(path);
-            if (suggestededDefinition == null)
-            {
-                var mapped = WPFUtil.MapExtensionToMain(path);
-                if (mapped != null)
-                    suggestededDefinition=manager.GetDefinitionByExtension("dummy"+mapped);
-
-            }
+        public void LoadCurrent(string path)   
+        {
+            var manager = HighlightingManager.Instance;
             
 
+//            if (path.HasContent() )
+ //               CurrentFileItem=  AllFileTypes.FirstOrDefault(p =>  path.EndsWith(p.FileExtension, StringComparison.OrdinalIgnoreCase));
 
-            if (suggestededDefinition != null) 
-                CurrentText.SyntaxHighlighting = suggestededDefinition;
+           string extension = Path.GetExtension(path);
+            
+            CurrentDefinition = manager.GetDefinitionByExtension(extension);
+            CurrentText = new TextEditor();
+
+          CurrentArea = CurrentText.TextArea;
+            CurrentArea.MinHeight = 500;
+            CurrentArea.MinWidth = 1000;
+            //CurrentText.SyntaxHighlighting = "C#";
+            //CurrentDocument= CurrentText.Document;
+      
+       
+
+
+            if (CurrentDefinition != null) 
+                CurrentText.SyntaxHighlighting = CurrentDefinition;
+
 
             CurrentPath = path; 
         }
