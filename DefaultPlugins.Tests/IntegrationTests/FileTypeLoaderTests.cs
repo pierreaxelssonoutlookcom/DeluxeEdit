7a7a6e792ddf733a65a4c1df4fdf085908bfbe09
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-01-24 23:01:50 +0100

    Tadded test file

---
+++ DefaultPlugins.Tests/IntegrationTests/FileTypeLoaderTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using DefaultPlugins;
+using Model;
+using Xunit;
+using System.IO;
+using DefaultPlugins.DefaultPlugins.PluginHelpers;
+
+namespace DeluxeEdit.DefaultPlugins.Tests.IntegrationTests
+{
+    public class FileTypeLoaderTests
+    {
+        public static string TestFile = "TestFiles/LogFileDefinition.xml";
+
+
+        [Fact]
+        public void LoadDefinitionTest()
+        {
+            var loader = new FileTypeLoader();
+            var definition = loader.LoadDefinitionFromFile(TestFile);
+
+
+            Assert.NotNull(definition);
+
+
+        }
+
+        [Fact]
+        public void RegisterDefinitionTest()
+        {
+            var loader = new FileTypeLoader();
+            var definition = loader.LoadDefinitionFromFile(TestFile);
+
+            loader.RegisterDefinition("LogFile",".log",definition);
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
